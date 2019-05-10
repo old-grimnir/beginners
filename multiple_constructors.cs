9f39e9fa0102857feb16202bc0eca644f9063206
old-grimnir <49686783+old-grimnir@users.noreply.github.com>
2019-05-10 21:12:38 +0100

    initial

---
+++ multiple_constructors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Scratch2name
+{
+    class Scratch2
+    {
+        public class Employee
+        {
+            public int Salary;
+
+            public Employee(int annualSalary)
+            {
+                Salary = annualSalary;
+            }
+
+            public Employee(int weeklySalary, int numberOfWeeks)
+            {
+                Salary = weeklySalary * numberOfWeeks;
+            }
+        }
+
+
+        static void Main()
+        {
+            Employee e1 = new Employee(30000);
+            Employee e2 = new Employee(500, 52);
+
+            Console.WriteLine(e1.Salary);
+            Console.WriteLine(e2.Salary);
+        }
+    }
+}
