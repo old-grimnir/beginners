69f864525d277615d17bcf03ebd70344f97f78da
old-grimnir <49686783+old-grimnir@users.noreply.github.com>
2019-05-17 14:01:48 +0100

    Add files via upload

---
+++ aggregate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Scratchname
+{
+    class Scratch
+    {
+        static void Main()
+        {
+            Console.WriteLine(GCD(new[] { 1605, 4200, 10235 }));
+        }
+
+        static int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+
+        static int GCD(int[] integerSet) // overloaded method
+        {
+            return integerSet.Aggregate(GCD); // similar to pythons reduce function
+        }
+    }
+}
